8e9efb2d6734b0ee7764ff84939007f1f0deeee7
Randy-kyle <randykyle.id@gmail.com>
2025-04-26 21:06:37 +0700

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,33 @@
+// backend/models/User.js
+
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', UserSchema);
